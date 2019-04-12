294bf5395a3457774e981f87b7531f585cd063cc
jarrahi_mm <jarrahi.mm@gmail.com>
2019-04-12 19:45:06 +0430

    :)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import controllers.Organizer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm Jarrahi!");
+        System.out.println("I'm Jarrahi!\nHello");
         Organizer organizer = new Organizer();
         organizer.main();
     }
