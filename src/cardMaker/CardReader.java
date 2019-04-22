62f76ff124d83a5f90f190f4b623550a605515fb
Ahmad Salimi <ahsalimi@ce.sharif.edu>
2019-04-22 14:44:59 +0430

    Json files updated carrying spell was moved from spell to spellAction using automatic editor made by me :)

--- src/cardMaker/CardReader.java
+++ src/cardMaker/CardReader.java
@@ -1,22 +1,42 @@
 package cardMaker;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import server.models.card.Card;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class CardReader {
 
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        String fileName = CardMaker.scanner.nextLine();
+        String directory = "jsonData/itemCards/usable";
+        File dir = new File(directory);
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                String name = file.getName();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(directory + "/" + name));
+
+                    Card loadedCard = new Gson().fromJson(bufferedReader, Card.class);
+
+                    writeAJsonFile(loadedCard, directory + "/" + name);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void writeAJsonFile(Card card, String address) {
+        String json = new GsonBuilder().setPrettyPrinting().create().toJson(card);
+        System.out.println(json);
 
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-
-            Card loadedCard = gson.fromJson(bufferedReader, Card.class);
+            FileWriter writer = new FileWriter(address);
+            writer.write(json);
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
