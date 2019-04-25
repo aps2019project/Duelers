3b25f682403c2da139b6767e3aa3eceb9e6ef18c
hadi <atomaton00@gmail.com>
2019-04-25 18:38:53 +0430

    debug leader board sorter add a new account

--- src/server/models/sorter/LeaderBoardSorter.java
+++ src/server/models/sorter/LeaderBoardSorter.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class LeaderBoardSorter implements Comparator<Account> {
     @Override
     public int compare(Account o1, Account o2) {
-        return o1.getWins() - o2.getWins();
+        return o2.getWins() - o1.getWins();
     }
 }
