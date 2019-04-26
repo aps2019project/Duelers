ac17784358f793d68af456ad7c695724f69585f2
Ahmad Salimi <ahsalimi@ce.sharif.edu>
2019-04-26 10:27:52 +0430

    tempStory added

---
+++ src/server/models/game/TempStory.java
@@ -0,0 +1,27 @@
+package server.models.game;
+
+import server.models.card.TempDeck;
+
+public class TempStory {
+    private TempDeck deck;
+    private GameType gameType;
+    private int reward;
+
+    public TempStory(Story story) {
+        this.deck = new TempDeck(story.getDeck());
+        this.gameType = story.getGameType();
+        this.reward = story.getReward();
+    }
+
+    public TempDeck getDeck() {
+        return deck;
+    }
+
+    public GameType getGameType() {
+        return gameType;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+}
