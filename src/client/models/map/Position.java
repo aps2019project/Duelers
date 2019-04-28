d3811274f0f21215ed1c9b442aac4e1b40d24fcf
hadi <atomaton00@gmail.com>
2019-04-28 12:47:31 +0000

    some game messages added

--- src/client/models/map/Position.java
+++ src/client/models/map/Position.java
@@ -9,6 +9,11 @@ public class Position {
 		this.column = cell.getColumn();
 	}
 
+	public Position(int row, int column){
+		this.row = row;
+		this.column = column;
+	}
+
 	public int getRow() {
 		return row;
 	}
