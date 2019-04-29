7d01c8e11a0c270aa534da56594b6350ccb64c1a
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-04-30 02:19:37 +0430

    change turn message

--- src/server/Server.java
+++ src/server/Server.java
@@ -694,6 +694,16 @@ public class Server {
                 (serverName, client2, cardId, newValue, 0));
     }
 
+    public void sendChangeTurnMessage(Game game, int newTurn) throws Exception {
+        String client1 = getClientName(game.getPlayerOne().getUserName());
+        String client2 = getClientName(game.getPlayerTwo().getUserName());
+        checkGameAccountsClient(client1, client2);
+        addToSendingMessages(Message.makeEndTurnMessage
+                (serverName, client1, newTurn, 0));
+        addToSendingMessages(Message.makeEndTurnMessage
+                (serverName, client2, newTurn, 0));
+    }
+
     private void readAccounts() {
         File[] files = new File(ACCOUNTS_PATH).listFiles();
         if (files != null) {
