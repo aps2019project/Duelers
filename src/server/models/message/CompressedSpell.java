17795b30d173ca60f1c4853190ae9e665dd9d9f3
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-05-02 02:55:32 +0430

    server compressedSpell constructor

--- src/server/models/message/CompressedSpell.java
+++ src/server/models/message/CompressedSpell.java
@@ -10,4 +10,13 @@ public class CompressedSpell {
     private int coolDown;
     private int mannaPoint;
     private int lastTurnUsed;
+
+    public CompressedSpell(String spellId, Target target, AvailabilityType availabilityType, int coolDown, int mannaPoint, int lastTurnUsed) {
+        this.spellId = spellId;
+        this.target = target;
+        this.availabilityType = availabilityType;
+        this.coolDown = coolDown;
+        this.mannaPoint = mannaPoint;
+        this.lastTurnUsed = lastTurnUsed;
+    }
 }
