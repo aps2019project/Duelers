b706d9d58ffca30cefb41c612c09c9ddd39b9a10
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-05-02 03:33:23 +0430

    CompressedGame server constructor

--- src/server/models/message/CompressedGame.java
+++ src/server/models/message/CompressedGame.java
@@ -1,8 +1,18 @@
 package server.models.message;
 
+import server.models.game.Player;
+import server.models.map.GameMap;
+
 public class CompressedGame {
     private CompressedPlayer playerOne;
     private CompressedPlayer playerTwo;
     private CompressedGameMap gameMap;
     private int turnNumber;
+
+    public CompressedGame(Player playerOne, Player playerTwo, GameMap gameMap, int turnNumber) {
+        this.playerOne = playerOne.toCompressedPlayer();
+        this.playerTwo = playerTwo.toCompressedPlayer();
+        this.gameMap = gameMap.toCompressedGameMap();
+        this.turnNumber = turnNumber;
+    }
 }
