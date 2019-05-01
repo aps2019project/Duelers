fc965169201ea4c63528324c6b850152706572a2
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-05-02 03:20:24 +0430

    CompressedPlayer server Constructor

--- src/server/models/message/CompressedPlayer.java
+++ src/server/models/message/CompressedPlayer.java
@@ -1,5 +1,7 @@
 package server.models.message;
 
+import server.models.card.Card;
+
 import java.util.ArrayList;
 
 public class CompressedPlayer {
@@ -11,4 +13,19 @@ public class CompressedPlayer {
     private ArrayList<CompressedCard> collectedItems = new ArrayList<>();
     private int playerNumber;
     private int numberOfCollectedFlags;
+
+    public CompressedPlayer(String userName, int currentMP, ArrayList<Card> hand, ArrayList<Card> graveyard,
+                            Card nextCard, ArrayList<Card> collectedItems, int playerNumber, int numberOfCollectedFlags) {
+        this.userName = userName;
+        this.currentMP = currentMP;
+        for (Card card : hand)
+            this.hand.add(card.toCompressedCard());
+        for (Card card : graveyard)
+            this.graveyard.add(card.toCompressedCard());
+        this.nextCard = nextCard.toCompressedCard();
+        for (Card card : collectedItems)
+            this.collectedItems.add(card.toCompressedCard());
+        this.playerNumber = playerNumber;
+        this.numberOfCollectedFlags = numberOfCollectedFlags;
+    }
 }
