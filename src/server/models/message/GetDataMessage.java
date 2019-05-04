026360983d6cb74ac3529935b4ee4e4670427841
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-05-04 05:14:39 +0400

    finish server and client messages

---
+++ src/server/models/message/GetDataMessage.java
@@ -0,0 +1,11 @@
+package server.models.message;
+
+import server.models.message.DataName;
+
+public class GetDataMessage {
+    private DataName dataName;
+
+    public DataName getDataName() {
+        return dataName;
+    }
+}
