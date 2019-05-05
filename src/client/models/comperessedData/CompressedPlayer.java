21e21aadd15aff76cca4c6b8238a378b9c13d2ee
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-05-05 05:30:40 +0400

    finish CompressedPlayer methods

--- src/client/models/comperessedData/CompressedPlayer.java
+++ src/client/models/comperessedData/CompressedPlayer.java
@@ -3,6 +3,7 @@ package client.models.comperessedData;
 import client.models.card.CardType;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CompressedPlayer {
     private String userName;
@@ -13,51 +14,77 @@ public class CompressedPlayer {
     private ArrayList<CompressedCard> collectedItems = new ArrayList<>();
     private int playerNumber;
     private int numberOfCollectedFlags;
-    private ArrayList<CompressedTroop> troops = new ArrayList<>(); // TODO: implementing needed;
-    private CompressedTroop hero; // TODO: implementing needed;
+    private ArrayList<CompressedTroop> troops = new ArrayList<>();
+    private CompressedTroop hero;
 
 
-    public void addCardToHand(CompressedCard card){
-
+    public void addCardToHand(CompressedCard card) {
+        hand.add(card);
+        if (hand.size() > 5)
+            System.out.println("Client Game Error!");
     }
 
-    public void addCardToNext(CompressedCard card){
-
+    public void addCardToNext(CompressedCard card) {
+        if (nextCard != null)
+            System.out.println("Client Game Error!");
+        else
+            nextCard = card;
     }
 
-    public void addCardToCollectedItems(CompressedCard card){
-
+    public void addCardToCollectedItems(CompressedCard card) {
+        collectedItems.add(card);
     }
 
-    public void addCardToGraveYard(CompressedCard card){
-
+    public void addCardToGraveYard(CompressedCard card) {
+        graveyard.add(card);
     }
 
     public void troopUpdate(CompressedTroop troop) {
-
+        if(troops==null)
+            troops=new ArrayList<>();
+        removeTroop(troop.getCard().getCardId());
+        troops.add(troop);
+        if (troop.getCard().getType() == CardType.HERO)
+            hero = troop;
     }
 
-    public void removeCardFromHand(String cardId){
-
+    public void removeCardFromHand(String cardId) {
+        Iterator<CompressedCard> iterator = hand.iterator();
+        while (iterator.hasNext()) {//TODO:Ahmad Check syntax
+            if (iterator.next().getCardId().equals(cardId))
+                iterator.remove();
+        }
     }
 
-    public void removeCardFromNext(String cardId){
-
+    public void removeCardFromNext(String cardId) {
+        if (nextCard.getCardId().equals(cardId)) {
+            nextCard = null;
+        } else {
+            System.out.println("Client Game Error!");
+        }
     }
 
-    public void removeCardFromCollectedItems(String cardId){
-
-    }
-
-    public void removeCardFromGraveYard(String cardId){
-
+    public void removeCardFromCollectedItems(String cardId) {
+        Iterator<CompressedCard> iterator = collectedItems.iterator();
+        while (iterator.hasNext()) {//TODO:Ahmad Check syntax
+            if (iterator.next().getCardId().equals(cardId))
+                iterator.remove();
+        }
     }
 
     public void removeTroop(String cardId) {
-
+        if(troops==null)
+            troops=new ArrayList<>();
+        Iterator<CompressedTroop> iterator = troops.iterator();
+        while (iterator.hasNext()) {//TODO:Ahmad Check syntax
+            if (iterator.next().getCard().getCardId().equals(cardId))
+                iterator.remove();
+        }
+        if (hero.getCard().getCardId().equals(cardId))
+            hero = null;
     }
 
-    public CompressedCard searchCard(String cardId) {
+    public CompressedCard searchHand(String cardId) {
         for (CompressedCard card : hand) {
             if (card.getCardId().equals(cardId)) {
                 return card;
