c54b63126c155fd5f84b945505bbbab1b3849207
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-06-03 18:15:04 +0430

    Server changing part 1

--- src/server/detaCenter/DataCenter.java
+++ src/server/detaCenter/DataCenter.java
@@ -1,4 +1,12 @@
 package server.detaCenter;
 
 public class DataCenter {
+    private static DataCenter ourInstance = new DataCenter();
+
+    public static DataCenter getInstance() {
+        return ourInstance;
+    }
+
+    private DataCenter() {
+    }
 }
