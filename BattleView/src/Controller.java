65cb8b0efd7f35540c536faca399e652e2606d8c
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-06-06 16:50:34 +0430

    Start Battle View

--- BattleView/src/Controller.java
+++ BattleView/src/Controller.java
@@ -1,4 +1,2 @@
-package sample;
-
 public class Controller {
 }
--- BattleView/src/Main.java
+++ BattleView/src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
