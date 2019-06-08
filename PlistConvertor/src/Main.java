0c91a6ead5701434865e063501e160e4f5da36ef
Ahmad Salimi <ahsalimi@ce.sharif.edu>
2019-06-09 00:52:50 +0430

    some changes in converter

--- PlistConvertor/src/Main.java
+++ PlistConvertor/src/Main.java
@@ -19,42 +19,44 @@ public class Main {
 
     public static void main(String[] args) {
         Converter converter;
-
-        System.out.println("Troop|Icon|Fx");
-
-        loop:
         while (true) {
-            switch (scanner.nextLine()) {
-                case "Troop":
-                    converter = new PlistToTroopPlaylistConverter();
-                    break loop;
-                case "Icon":
-                    converter = new PlistToIconPlaylistConverter();
-                    break loop;
-                case "Fx":
-                    converter = new PlistToFxPlaylistConverter();
-                    break loop;
-                default:
-                    System.out.println("Error!");
-            }
-        }
+            System.out.println("Troop|Icon|Fx|poweroff");
 
-        File directory = new File(converter.getPath() + converter.getInitialFolder());
-        File[] files = directory.listFiles();
-
-        if (files != null) {
-            for (File file : files) {
-                try {
-                    ArrayList<Plist> list = gson.fromJson(new FileReader(file), listType);
-
-                    write(
-                            converter.getPath() + converter.getDestinationFolder() + "/" + file.getName(),
-                            gson.toJson(converter.convert(list.get(0)))
-                    );
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+            loop:
+            while (true) {
+                switch (scanner.nextLine()) {
+                    case "Troop":
+                        converter = new PlistToTroopPlaylistConverter();
+                        break loop;
+                    case "Icon":
+                        converter = new PlistToIconPlaylistConverter();
+                        break loop;
+                    case "Fx":
+                        converter = new PlistToFxPlaylistConverter();
+                        break loop;
+                    case "poweroff":
+                        return;
+                    default:
+                        System.out.println("Error!");
                 }
+            }
 
+            File directory = new File(converter.getPath() + converter.getInitialFolder());
+            File[] files = directory.listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    try {
+                        ArrayList<Plist> list = gson.fromJson(new FileReader(file), listType);
+
+                        write(
+                                converter.getPath() + converter.getDestinationFolder() + "/" + file.getName(),
+                                gson.toJson(converter.convert(list.get(0)))
+                        );
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
