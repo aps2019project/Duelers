0befeca005744a67637ba791c96e1121c8ef2b18
Ahmad Salimi <ahsalimi@ce.sharif.edu>
2019-06-11 11:56:55 +0430

    multiplayer menu server requests done!

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         GraphicalUserInterface.getInstance().start(stage);
     }
 }
