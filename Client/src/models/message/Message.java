753f275a34102539a6ca15db0cc285e253064c4f
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-07-07 10:41:31 +0430

    makeAcceptRequestMessage

--- Client/src/models/message/Message.java
+++ Client/src/models/message/Message.java
@@ -217,6 +217,14 @@ public class Message {
         return message;
     }
 
+    public static Message makeAcceptRequestMessage(String sender, String receiver, String opponentUsername) {
+        Message message = new Message(sender, receiver, 0);
+        message.messageType = MessageType.ACCEPT_REQUEST;
+        message.newGameFields=new NewGameFields();
+        message.newGameFields.setOpponentUsername(opponentUsername);
+        return message;
+    }
+
     public static Message makeNewCustomGameMessage(String sender, String receiver, GameType gameType, int numberOfFlags, String customDeckName, int messageId) {
         Message message = new Message(sender, receiver, messageId);
         message.newGameFields = new NewGameFields();
