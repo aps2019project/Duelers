d218b24aca63c80f3cde84683fc5df031911b820
hadi <atomaton00@gmail.com>
2019-07-11 21:03:47 +0430

    Rest getAllKeys

--- Server/src/server/dataCenter/models/db/Rest.java
+++ Server/src/server/dataCenter/models/db/Rest.java
@@ -2,6 +2,7 @@ package server.dataCenter.models.db;
 
 import io.joshworks.restclient.http.HttpResponse;
 import io.joshworks.restclient.http.Unirest;
+import server.clientPortal.models.JsonConverter;
 import server.dataCenter.DataBase;
 import server.dataCenter.models.account.Collection;
 import server.dataCenter.models.card.Card;
@@ -11,26 +12,51 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Rest implements DataBase {
-    private String[] maps = {"originalCards","customCards","collectibleItems","stories","originalFlag"};
-    Rest(){
+    private String[] maps = {"originalCards", "customCards", "collectibleItems", "stories", "originalFlag"};
+    final String baseAddress = "http://127.0.0.1:8080/";
+
+    public static void main(String[] args) {
+        Rest rest = new Rest();
+    }
+
+    Rest() {
         for (String s : maps) {
             createMap(s);
         }
     }
 
     private void createMap(String name) {
-        final String baseAddress = "http://127.0.0.1:8080/";
         final String path = "init_DB";
-        HttpResponse<String> response= null;
+        HttpResponse<String> response = null;
         HashMap<String, Object> parameters = new HashMap<>();
         parameters.put("name", name);
         try {
             response = Unirest.post(baseAddress + path)
                     .fields(parameters)
                     .asString();
+            if (response.getStatus()==200)
+                System.out.println("init db: "+name);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private List getAllKeys(String name){
+        final String path = "get_all_keys";
+        HttpResponse<String> response = null;
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("name", name);
+        try {
+            response = Unirest.post(baseAddress + path)
+                    .fields(parameters)
+                    .asString();
+            if (response.getStatus()==200)
+                return JsonConverter.fromJson(response.getBody(),List.class);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
