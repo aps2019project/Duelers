a25cf88d5d24ae2a263eefde42d7ec65ca0c074d
hadi <atomaton00@gmail.com>
2019-07-11 15:05:26 +0430

    Rest initialize

--- Server/src/server/dataCenter/models/db/Rest.java
+++ Server/src/server/dataCenter/models/db/Rest.java
@@ -1,13 +1,38 @@
 package server.dataCenter.models.db;
 
+import io.joshworks.restclient.http.HttpResponse;
+import io.joshworks.restclient.http.Unirest;
 import server.dataCenter.DataBase;
 import server.dataCenter.models.account.Collection;
 import server.dataCenter.models.card.Card;
 import server.gameCenter.models.game.Story;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Rest implements DataBase {
+    private String[] maps = {"originalCards","customCards","collectibleItems","stories","originalFlag"};
+    Rest(){
+        for (String s : maps) {
+            createMap(s);
+        }
+    }
+
+    private void createMap(String name) {
+        final String baseAddress = "http://127.0.0.1:8080/";
+        final String path = "init_DB";
+        HttpResponse<String> response= null;
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("name", name);
+        try {
+            response = Unirest.post(baseAddress + path)
+                    .fields(parameters)
+                    .asString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Card getCard(String cardName) {
         return null;
